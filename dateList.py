390b1d9d581aa64ae4be1f2fdf851722e75c480b
안수빈 <102291165+dupe-in@users.noreply.github.com>
2022-07-22 14:37:09 +0900

    Update dateList.py

--- dateList.py
+++ dateList.py
@@ -1,23 +1,24 @@
 import numpy as np
 
 # 8자리 시작, 마지막 날짜 입력
-startDate = "20220102"
-lastDate = "20221231"
+start_date = "20220102"
+end_date = "20220105"
 
-syear = startDate[0:4]
-smonth = startDate[4:6]
-sday = startDate[6:]
+syear = start_date[0:4]
+smonth = start_date[4:6]
+sday = start_date[6:]
 strStartDate = syear + "-" + smonth + "-" + sday
 
-lyear = lastDate[0:4]
-lmonth = lastDate[4:6]
-lday = lastDate[6:]
+lyear = end_date[0:4]
+lmonth = end_date[4:6]
+lday = end_date[6:]
 strLastDate = lyear + "-" + lmonth + "-" + lday
 
 dateStartDate = np.array(strStartDate, dtype=np.datetime64)
 dateLastDate = np.array(strLastDate, dtype=np.datetime64)
 c = dateLastDate - dateStartDate
 
-resultList = [dateStartDate + np.arange(c + 1)]
-
-print(resultList[0])
+tempList = [dateStartDate + np.arange(c + 1)]
+resultList = tempList[0]
+resultList = resultList.tolist()
+print(resultList)
